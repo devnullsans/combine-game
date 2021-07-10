c22d2f1e2c220b8bb4f44fdf472cc0e3d036fe21
Paul <52319567+devnullsans@users.noreply.github.com>
2021-07-10 18:52:02 +0530

    Change and rename main javascript file
    Renamed refactor.js to logic.js and added logics for save-game and load-game functionality.

--- logic.js
+++ logic.js
@@ -4,20 +4,39 @@ const dropD = document.querySelectorAll('div.dropD');
 const buttonL = document.getElementById('lvlbtn');
 const buttonT = document.getElementById('tmrbtn');
 const tempMap = ['img/afghanistan.png', 'img/albania.png', 'img/algeria.png', 'img/andora.png', 'img/angola.png', 'img/argentina.png', 'img/armenia.png', 'img/australia.png', 'img/austria.png', 'img/bahrain.png', 'img/bangladesh.png', 'img/barbados.png', 'img/belgium.png', 'img/bostwana.png', 'img/brazil.png', 'img/bulgaria.png', 'img/cameroon.png', 'img/canada.png', 'img/chile.png', 'img/china.png', 'img/colombia.png', 'img/costarica.png', 'img/croatia.png', 'img/cuba.png', 'img/cyprus.png', 'img/czech.png', 'img/denmark.png', 'img/ecuador.png', 'img/egypt.png', 'img/esperanto.png', 'img/estonia.png', 'img/ethiopia.png', 'img/europe.png', 'img/finland.png', 'img/france.png', 'img/georgia.png', 'img/germany.png', 'img/ghana.png', 'img/greece.png', 'img/honduras.png', 'img/hongkong.png', 'img/hungary.png', 'img/iceland.png', 'img/india.png', 'img/indonesia.png', 'img/irak.png', 'img/iran.png', 'img/ireland.png', 'img/israel.png', 'img/italy.png', 'img/ivory.png', 'img/jamaica.png', 'img/japan.png', 'img/kenya.png', 'img/korea.png', 'img/koweit.png', 'img/laos.png', 'img/latvia.png', 'img/lebanon.png', 'img/liechtenstein.png', 'img/lithuania.png', 'img/luxembourg.png', 'img/lybia.png', 'img/malaysia.png', 'img/malta.png', 'img/mexico.png', 'img/morocco.png', 'img/movies.png', 'img/mozambique.png', 'img/music.png', 'img/namibia.png', 'img/nepal.png', 'img/netherlands.png', 'img/newzealand.png', 'img/nicaragua.png', 'img/nigeria.png', 'img/norway.png', 'img/pakistan.png', 'img/panama.png', 'img/paraguay.png', 'img/peru.png', 'img/philippines.png', 'img/poland.png', 'img/portugal.png', 'img/romania.png', 'img/russia.png', 'img/rw.png', 'img/saudi.png', 'img/scotland.png', 'img/senegal.png', 'img/serbia.png', 'img/singapore.png', 'img/slovakia.png', 'img/slovenia.png', 'img/south-africa.png', 'img/spain.png', 'img/sweden.png', 'img/switzerland.png', 'img/syria.png', 'img/taiwan.png', 'img/tanzania.png', 'img/thailand.png', 'img/togo.png', 'img/tunisia.png', 'img/turkey.png', 'img/uganda.png', 'img/uk.png', 'img/ukraine.png', 'img/uruguay.png', 'img/usa.png', 'img/uzbekistan.png', 'img/venezuela.png', 'img/vietnam.png', 'img/wales.png', 'img/yemen.png', 'img/zambia.png', 'img/zimbabwe.png'];
-const trails = ['K', 'M', 'B', 'T', 'Qd', 'Qn', 'Sx', 'Sp', 'O', 'N', 'D', 'Ud', 'Dd', 'Td', 'Qtd', 'Qnd', 'Sxd', 'Spd', 'Od', 'Nd', 'V'];
-const gameState = {
-    coins: 0,
-    level: 0,
-    next: 1000,
-    less: 20000,
-    maxTime: 6000,
-    runTime: 0,
-    spawnID: -1,
-    countID: -1,
-    refTick: 50
-};
-let heldI = null;
+const trails = ['M', 'B', 'T', 'Qd', 'Qn', 'Sx', 'Sp', 'O', 'N', 'D', 'Ud', 'Dd', 'Td', 'Qtd', 'Qnd', 'Sxd', 'Spd', 'Od', 'Nd', 'V'];
 
+function initState() {
+    return JSON.parse(localStorage.getItem('save')) ?? {
+        coins: 0,
+        level: 0,
+        next: 1000,
+        less: 20000,
+        time: 6000,
+        running: 0,
+        spawnID: -1,
+        countID: -1,
+        refresh: 50
+    };
+}
+function initStatic() {
+    const dival = JSON.parse(localStorage.getItem('divs'));
+    for (const ind of dropD.keys()) {
+        if (dival && dival[ind]) newBin(Number(dival[ind]), dropD[ind]);
+        dropD[ind].onanimationend = onAnimationEnd;
+        dropD[ind].ondragover = onDragOver;
+        dropD[ind].ondrop = onDrop;
+    }
+    timerP.max = gameState.time;
+    timerP.value = gameState.running;
+    buttonL.dataset.req = moneyFormat(gameState.next);
+    buttonL.onclick = onClickLevel;
+    buttonT.dataset.req = moneyFormat(gameState.less);
+    buttonT.onclick = onClickTimer;
+    gameState.spawnID = setInterval(onSpawnTick, gameState.refresh);
+    gameState.countID = setInterval(onCountTick, 1000);
+    onbeforeunload = onBeforeUnload;
+}
 function moneyFormat(value) {
     const amount = value.toString();
     const length = amount.length;
@@ -30,16 +49,18 @@ function moneyFormat(value) {
         else return `$${amount.substring(0, 3)}.${amount.substring(3, 6)}${trails[int - 1]}`;
     }
 }
-function newBin(level) {
+function newBin(level, div) {
     const img = document.createElement('img');
     img.setAttribute('src', tempMap[level]);
     img.setAttribute('alt', level);
-    heldI = document.createElement('figure');
-    heldI.setAttribute('id', Date.now().toString(36));
-    heldI.setAttribute('data-flash', moneyFormat(3 ** level));
-    heldI.setAttribute('draggable', true);
-    heldI.ondragstart = onDragStart;
-    heldI.appendChild(img);
+    const figure = document.createElement('figure');
+    figure.setAttribute('id', (Math.random() * 100 | 0).toString(36) + performance.now().toString(36));
+    figure.setAttribute('data-flash', moneyFormat(3 ** level));
+    figure.setAttribute('draggable', true);
+    figure.ondragstart = onDragStart;
+    figure.appendChild(img);
+    if (div) div.appendChild(figure);
+    else heldI = figure;
 }
 function addBin() {
     let empty = null;
@@ -55,13 +76,13 @@ function addBin() {
     } else return false;
 }
 function onSpawnTick() {
-    timerP.value = gameState.runTime;
-    if (gameState.runTime >= gameState.maxTime) {
-        gameState.runTime = 0;
+    timerP.value = gameState.running;
+    if (gameState.running >= gameState.time) {
+        gameState.running = 0;
         newBin(gameState.level);
         if (!addBin()) clearInterval(gameState.spawnID);
     } else {
-        gameState.runTime += gameState.refTick;
+        gameState.running += gameState.refresh;
     }
 }
 function onCountTick() {
@@ -75,7 +96,10 @@ function onClickLevel(ev) {
     if (gameState.coins >= gameState.next) {
         const level = ++gameState.level;
         total.textContent = moneyFormat(gameState.coins -= gameState.next);
-        ev.target.dataset.req = moneyFormat(gameState.next *= 2);
+        if (gameState.level == 116) {
+            ev.target.disabled = true;
+            ev.target.dataset.req = 'MAX';
+        } else ev.target.dataset.req = moneyFormat(gameState.next *= 2);
         for (const div of dropD)
             if (div.childElementCount > 0 && div.lastElementChild.lastElementChild.alt < level) {
                 div.lastElementChild.lastElementChild.alt = level;
@@ -91,13 +115,21 @@ function onClickLevel(ev) {
 }
 function onClickTimer(ev) {
     if (gameState.coins >= gameState.less) {
-        timerP.max = gameState.maxTime -= 100;
+        timerP.max = gameState.time -= 100;
         total.textContent = moneyFormat(gameState.coins -= gameState.less);
-        ev.target.dataset.req = moneyFormat(gameState.less *= 4);
+        if (gameState.time == 2000) {
+            ev.target.disabled = true;
+            ev.target.dataset.req = 'MAX';
+        } else ev.target.dataset.req = moneyFormat(gameState.less *= 4);
     }
 }
+function onBeforeUnload() {
+    gameState.spawnID = gameState.countID = clearInterval(gameState.spawnID) ?? clearInterval(gameState.countID) ?? -1;
+    localStorage.setItem('divs', JSON.stringify(Array.prototype.map.call(dropD, div => div.lastElementChild?.lastElementChild.alt ?? null)));
+    localStorage.setItem('save', JSON.stringify(gameState));
+}
 function onAnimationEnd(ev) {
-    ev.srcElement.classList.toggle('show');
+    ev.srcElement.classList.remove('show');
 }
 function onDragOver(ev) {
     ev.preventDefault();
@@ -120,20 +152,10 @@ function onDrop(ev) {
         const num = ++img.lastElementChild.alt;
         img.lastElementChild.src = tempMap[num];
         img.dataset.flash = moneyFormat(3 ** num);
-        if (heldI && addBin()) gameState.spawnID = setInterval(onSpawnTick, gameState.refTick);
+        if (heldI && addBin()) gameState.spawnID = setInterval(onSpawnTick, gameState.refresh);
     }
 }
 
-for (const div of dropD) {
-    div.onanimationend = onAnimationEnd;
-    div.ondragover = onDragOver;
-    div.ondrop = onDrop;
-}
-timerP.max = gameState.maxTime;
-timerP.value = gameState.runTime;
-buttonL.dataset.req = moneyFormat(gameState.next);
-buttonL.onclick = onClickLevel;
-buttonT.dataset.req = moneyFormat(gameState.less);
-buttonT.onclick = onClickTimer;
-gameState.spawnID = setInterval(onSpawnTick, gameState.refTick);
-gameState.countID = setInterval(onCountTick, 1000);
+let heldI = null;
+const gameState = initState();
+initStatic();
